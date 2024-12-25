5852a4fa374a659874fc3b507049fe094959067c
Martín Pizarro <martinpiz097@gmail.com>
2024-12-25 08:20:00 -0300

    wallpaper change

--- dotfiles/dot_config/hypr/scripts/maximize_all.sh
+++ dotfiles/dot_config/hypr/scripts/maximize_all.sh
@@ -11,9 +11,10 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-workspace=$1
-current_window=$(hyprctl clients -j | jq -r "[.[] | select(.workspace.id == $workspace)] | .[-1]" | grep -v '^$')
+current_workspace=cat $(hyprctl -j activeworkspace | jq -r ".id")
+current_window=$(hyprctl clients -j | jq -r "[.[] | select(.workspace.id == $current_workspace)] | .[-1]" | grep -v '^$')
 current_window_maximized=$(echo "$current_window" | jq -r ".fullscreen == $maximized_state" | grep -v '^$')
+another_windows
 
 
 if [[ $(echo "$current_window_maximized" | xargs) == "true" ]]; then
