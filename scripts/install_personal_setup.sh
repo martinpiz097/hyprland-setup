a46495f78787540f98658b08454aa6857d519186
Martín Pizarro <martinpiz097@gmail.com>
2024-12-31 11:40:36 -0300

    many changes and new rules

--- scripts/install_personal_setup.sh
+++ scripts/install_personal_setup.sh
@@ -11,10 +11,21 @@ ROOT_FOLDER=$REPO_FOLDER/scripts/personal-setup-scripts
 #SET_WALLPAPER_SCRIPT=$REPO_FOLDER/scripts/set-wallpaper.sh
 WALLPAPERS_PATH=$REPO_FOLDER/wallpapers
 
+echo "Comprobando carpeta wallapers..."
+
 if ! [ -d $WALLPAPERS_PATH ]; then
+	echo "Carpeta wallpapers no existe, descargando..."	
 	cd $REPO_FOLDER
 	git clone https://github.com/martinpiz097/hyprland-wallpapers wallpapers
+else 
+	echo "Carpeta wallpapers existe, actualizando..."	
+	cd $REPO_FOLDER/wallpapers
+	git reset --hard
+	git pull
 fi
+
+echo "Instalando hapless para procesos paralelos..."
+pipx install hapless
 	
 sh $ROOT_FOLDER/clear_current_setup.sh
 sh $ROOT_FOLDER/load_personal_setup.sh
