d0652c873acf2769775c4cad19eba20d59f82473
Martín Pizarro <martinpiz097@gmail.com>
2025-02-14 23:30:26 -0300

    fix(panel): Set icon for network executor on indicators panel

--- scripts/nwg-panel/show-active-iface-info.sh
+++ scripts/nwg-panel/show-active-iface-info.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-nmcli -g IP4.ADDRESS device show $(nmcli -g DEVICE connection show --active | head -n1)
+ip_addr=$(nmcli -g IP4.ADDRESS device show $(nmcli -g DEVICE connection show --active | head -n1))
+
+echo /usr/share/gopsuinfo/icons_light/net.svg
+echo $ip_addr
